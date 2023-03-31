601ecabafcd67b8d5178b89a568f14b8f6037488
KSerg2022 <koshevoys@gmail.com>
2023-03-31 19:34:13 +0200

    refactored file 'run'.
    Created class 'WikiRacer'.

--- run.py
+++ run.py
@@ -6,16 +6,22 @@ from main.pathfinder import PathFinder
 from utils.calc_time import calc_time
 
 
-@calc_time
-def main(start_article, finish_article, requests_per_minute, urls_per_page):
-    """Main controller"""
-    path = PathFinder(start_article, finish_article, requests_per_minute, urls_per_page)
-    path_from_to = path.main()
-    if not path_from_to:
-        print('Can not find result.', path_from_to)
-    print(path_from_to)
+class WikiRacer:
+    @calc_time
+    def find_path(self, start_article: str, finish_article: str, requests_per_minute, urls_per_page):
+        path = PathFinder(start_article=start_article,
+                          finish_article=finish_article,
+                          requests_per_minute=requests_per_minute,
+                          urls_per_page=urls_per_page)
+        path_from_to = path.main()
+        if not path_from_to:
+            print('Can not find result.', path_from_to)
+            return path_from_to
+        print(path_from_to)
+        return path_from_to
 
 
 if __name__ == '__main__':
     for articles in tasks:
-        main(*articles, requests_per_minute=requests_per_minute, urls_per_page=urls_per_page)
+        path = WikiRacer()
+        path.find_path(*articles, requests_per_minute=requests_per_minute, urls_per_page=urls_per_page)
