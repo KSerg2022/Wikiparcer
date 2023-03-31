c8cc7f805f3cdaa118f362dd24e95c678263edf3
KSerg2022 <koshevoys@gmail.com>
2023-03-31 19:35:33 +0200

    refactored file 'wikiracing_test.py'.

--- wikiracing_test.py
+++ wikiracing_test.py
@@ -1,34 +1,35 @@
 import unittest
 
-from wikiracing import WikiRacer
+from run import WikiRacer
+from settings import requests_per_minute, urls_per_page
 
 
 class WikiRacerTest(unittest.TestCase):
     racer = WikiRacer()
 
     def test_1(self):
-        path = self.racer.find_path('Дружба', 'Рим')
+        path = self.racer.find_path('Дружба', 'Рим', requests_per_minute, urls_per_page)
         self.assertEqual(path, ['Дружба', 'Якопо Понтормо', 'Рим'])
 
     def test_2(self):
-        path = self.racer.find_path('Мітохондріальна ДНК', 'Вітамін K')
+        path = self.racer.find_path('Мітохондріальна ДНК', 'Вітамін K', requests_per_minute, urls_per_page)
         self.assertEqual(path, ['Мітохондріальна ДНК', 'Бактерії', 'Вітамін K'])
 
     def test_3(self):
-        path = self.racer.find_path('Марка (грошова одиниця)', 'Китайський календар')
-        self.assertIn('Марка (грошова одиниця)', path)
-        self.assertIn('19', path[1])
-        self.assertIn('Китайський календар', path)
+        path = self.racer.find_path('Марка (грошова одиниця)', 'Китайський календар', requests_per_minute, urls_per_page)
+        self.assertEqual(path, ['Марка (грошова одиниця)', '2017', 'Китайський календар'])
 
     def test_4(self):
-        path = self.racer.find_path('Фестиваль', 'Пілястра')
+        path = self.racer.find_path('Фестиваль', 'Пілястра', requests_per_minute, urls_per_page)
         self.assertEqual(path, ['Фестиваль', 'Бароко', 'Пілястра'])
 
     def test_5(self):
-        path = self.racer.find_path('Дружина (військо)', '6 жовтня')
-        self.assertIn('Дружина (військо)', path)
-        self.assertIn('світова війна', path[1])
-        self.assertIn('6 жовтня', path)
+        path = self.racer.find_path('Дружина (військо)', '6 жовтня', requests_per_minute, urls_per_page)
+        self.assertEqual(path, ['Дружина (військо)', 'Перша світова війна', '6 жовтня'])
+
+    def test_6(self):
+        path = self.racer.find_path('Україна', 'Азовсталь', requests_per_minute, urls_per_page)
+        self.assertEqual(path, ['Україна', 'Маріуполь', 'Азовсталь'])
 
 
 if __name__ == '__main__':
